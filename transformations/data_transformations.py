c5bca5f80c5e78cc7f5a7c319d377303998471f1
RISHIKAREDDY2000 <rishikareddy6188@gmail.com>
2026-03-31 11:59:54 -0400

    Update data_transformations.py

--- transformations/data_transformations.py
+++ transformations/data_transformations.py
@@ -1,1 +1,10 @@
+from pyspark.sql.functions import col, when
 
+def flag_high_value_transactions(df):
+
+    df = df.withColumn(
+        "high_value_flag",
+        when(col("amount") > 200, 1).otherwise(0)
+    )
+
+    return df
