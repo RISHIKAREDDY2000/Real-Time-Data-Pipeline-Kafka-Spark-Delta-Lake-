5d3ea234cdab056c86044d82361d6901d0402f09
RISHIKAREDDY2000 <rishikareddy6188@gmail.com>
2026-03-31 12:00:17 -0400

    Update create_delta_tables.sql

--- delta_tables/create_delta_tables.sql
+++ delta_tables/create_delta_tables.sql
@@ -1,1 +1,10 @@
+CREATE TABLE transactions_delta
+USING DELTA
+LOCATION '/delta/transactions';
 
+CREATE TABLE fraud_transactions
+USING DELTA
+AS
+SELECT *
+FROM transactions_delta
+WHERE amount > 200;
