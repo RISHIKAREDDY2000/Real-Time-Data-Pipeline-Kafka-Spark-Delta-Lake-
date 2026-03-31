09e96250fb0f84a2a834bf6ea57b0b64d3a5df89
RISHIKAREDDY2000 <rishikareddy6188@gmail.com>
2026-03-31 11:59:10 -0400

    Update spark_streaming_pipeline.py

--- streaming_pipeline/spark_streaming_pipeline.py
+++ streaming_pipeline/spark_streaming_pipeline.py
@@ -1,1 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
+spark = SparkSession.builder \
+    .appName("FraudDetectionPipeline") \
+    .getOrCreate()
+
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("date", StringType()),
+    StructField("client_id", IntegerType()),
+    StructField("card_id", IntegerType()),
+    StructField("amount", DoubleType()),
+    StructField("merchant_id", IntegerType()),
+    StructField("merchant_city", StringType()),
+    StructField("merchant_state", StringType()),
+    StructField("zip", IntegerType()),
+    StructField("mcc", IntegerType())
+])
+
+df = spark \
+.readStream \
+.format("kafka") \
+.option("kafka.bootstrap.servers","localhost:9092") \
+.option("subscribe","transactions_topic") \
+.load()
+
+transactions = df.selectExpr("CAST(value AS STRING)") \
+.select(from_json(col("value"), schema).alias("data")) \
+.select("data.*")
+
+transactions.writeStream \
+.format("delta") \
+.option("checkpointLocation","/tmp/checkpoints") \
+.start("/delta/transactions")
